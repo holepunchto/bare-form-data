3a3e9f034d26a60cc91df65afb41760c3ad3d0fc
Yasser Nascimento <dev@yasser.email>
2025-10-14 17:40:56 -0300

    Switch to `prettier-config-holepunch`

--- test.js
+++ test.js
@@ -5,10 +5,7 @@ test('basic', (t) => {
   const form = new FormData()
 
   form.append('title', 'Hello form')
-  form.append(
-    'attachment',
-    new File(['My attachment'], 'attachment.txt', { type: 'text/plain' })
-  )
+  form.append('attachment', new File(['My attachment'], 'attachment.txt', { type: 'text/plain' }))
 
   t.ok(form)
 
